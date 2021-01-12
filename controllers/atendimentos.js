8c6cdf6edcff5b918ad503dc518a4b778a944400
Matheus <matheusgalvao03@gmail.com>
2021-01-11 21:31:06 -0300

    Levantando banco de dados Mysql e funcionando com o servidor!

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,9 @@
 module.exports = app =>  {
     app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos!'))
 
-    app.post('/atendimentos', (req, res)=> res.send('Voce está na rota de atendimentos e está realizando um poste'))
+    app.post('/atendimentos', (req, res)=> {
+        console.log(req.body)
+
+        res.send('Voce está na rota de atendimentos e está realizando um poste')
+    })
 }   
